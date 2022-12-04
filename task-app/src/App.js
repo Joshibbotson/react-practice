c2772cd77cc0624bc8d2d223c9282b24b4dd7078
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-12-04 16:47:00 +0000

    tasks can now be successfully added and will be numbered too

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,27 +1,64 @@
 import "./App.css"
+// this imports react and react components, note {Component} is destructing so we
+// can using class App extends Component rather than class App extend React.Component//
 import React, { Component } from "react"
-import overview from "./components/Overview"
+import Overview from "./components/Overview"
+// this important a library for unique id's using npm install uniqid
+//this is useful because normally databases give you a unique ID//
+import uniqid from "uniqid"
 
+//now we extend to get access to all components properties
 class App extends Component {
     constructor() {
         super()
-
+        // note state has been defined within the constructor//
         this.state = {
-            task: { text: "" },
+            task: { text: "", id: uniqid(), no: 0 },
             tasks: [],
         }
     }
 
+    handleChange = e => {
+        console.log(e.target.value)
+        this.setState({
+            task: {
+                text: e.target.value,
+                id: this.state.task.id,
+                no: this.state.tasks.length,
+            },
+        })
+        console.log(this.state)
+    }
+
+    onSubmitTask = e => {
+        e.preventDefault()
+        this.setState({
+            tasks: this.state.tasks.concat(this.state.task),
+            task: { text: "" },
+            id: uniqid(),
+        })
+    }
+
     render() {
-        const { task, tasks } = this.state
+        const { task, tasks } = this.state //destructed state.task state.tasks so can now
+        //be used as task and tasks//
 
         return (
             <div>
-                <form>
+                <form onSubmit={this.onSubmitTask}>
                     <label htmlFor="taskInput"> Enter task</label>
-                    <input type="text" id="taskInput" />
-                    <button type="submit">Add Task</button>
+                    <input
+                        onChange={this.handleChange}
+                        value={task.text} //we need to specify what the value output should
+                        // look like in react//
+                        type="text"
+                        id="taskInput"
+                    />
+                    <button type="submit" onClick={this.onSubmitTask}>
+                        Add Task
+                    </button>
                 </form>
+                <Overview tasks={tasks} />
             </div>
         )
     }
--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,9 +1,25 @@
-import App from "../App"
+// overview function is a component//
 
-export default function overview({ task }) {
+import React from "react"
+
+const Overview = props => {
+    const { tasks } = props //destructing so no longer need props.tasks//
+
+    //simply uses .map to create a <li> item for each task within a new array, it returns
+    // a new array with all of these list items//
     return (
-        <div className="task-container">
-            <h1 className="task">{task}</h1>
-        </div>
+        <ul>
+            {tasks.map(task => {
+                return (
+                    <li key={task.id}>
+                        {task.no}
+                        {") "}
+                        {task.text}
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
+
+export default Overview
