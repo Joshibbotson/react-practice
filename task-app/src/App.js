df52a8df7ef562a038640b0474a7be26206aeca9
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-12-12 18:41:37 +0000

    updated

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,26 +1,40 @@
 import "./App.css"
 import React, { useState } from "react"
 import ReactDOM from "react-dom"
-// import uniqid from "uniqid"
+import uniqid from "uniqid"
 const App = () => {
+    const [state, setState] = useState({
+        currentTask: {
+            text: "",
+        },
+        taskArr: [{ text: "todo1" }],
+    })
+
     //component//
     const Input = props => {
         console.log(props)
         return (
             /*element*/ <div>
-                <input id="textInput" type="text"></input>
-                <button onClick={handleClick.bind(todos, setTodos)}>
-                    {props.searchName}
-                </button>
+                <input
+                    id="textInput"
+                    type="text"
+                    onChange={handleChange.bind()}
+                ></input>
+                <button onClick={handleClick.bind()}>{props.searchName}</button>
                 <p>{props.someText}</p>
             </div>
         )
     }
 
-    const handleClick = (state, setState) => {
-        const textInput = document.getElementById("textInput")
-        console.log(textInput.value)
+    const handleChange = () => {
+        // setTasks({
+        //     text: value,
+        //     no: 1,
+        //     id: uniqid(),
+        // })
     }
+
+    const handleClick = () => {}
 
     const Task = props => {
         return (
@@ -34,17 +48,12 @@ const App = () => {
         text: ["b", "c", "d"],
     }
 
-    const [todos, setTodos] = useState([
-        { id: 1, name: "Todo 1", complete: false },
-        { id: 2, name: "Todo 2", complete: false },
-        { id: 3, name: "Todo 3", complete: false },
-    ])
-
     return (
         <div>
             <Input searchName="Add Task" someText="Lover of todos" />
-            {todos.map(task => {
-                return <Task todos={task.name} />
+
+            {state.taskArr.map(task => {
+                return <Task todos={task.text} />
             })}
         </div>
     )
